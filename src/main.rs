ddba9000d88d736cb34a7559ca93ea1b7ab945be
Madelyn-of-Hell <116776146+Madelyn-of-Hell@users.noreply.github.com>
2025-03-02 19:49:29 +1100

    Green turn implemented.Not fully effective. Requires further testing. May require fine tuning.

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ impl Side {
 // The speed value. Listed as 500 - {val} where val is how fast you go - and the point at which your speed becomes negative.
 const SPEED:i32 = 500 - 400;
 const ADJUSTMENT_VALUE:i32 = 125;
+const GREEN_OFFSET:i32 = 75;
 fn main() -> Ev3Result<()> {
     // Declare Sensors
     let (left_sensor, right_sensor) = (ColorSensor::get(SensorPort::In2)?, ColorSensor::get(SensorPort::In1)?);
@@ -44,6 +45,23 @@ fn main() -> Ev3Result<()> {
         if left_speed-adjustment_val < 0 && right_speed-adjustment_val < 0 {
             adjustment_val = ADJUSTMENT_VALUE;
         } else { adjustment_val = 0; }
+
+        // Returns the side that a green value is detected on or none if one isn't detected.
+        let green:Option<Side> =
+                 if left_sensor.get_green()?  > GREEN_OFFSET + left_sensor.get_red()? {Some(Side::Left)}
+            else if right_sensor.get_green()?  > GREEN_OFFSET + right_sensor.get_red()? {Some(Side::Right)}
+            else { None };
+
+        // Discards None values, then turns around the sensor that detected green.
+        // TODO: Add a timeout value to prevent getting stuck around green squares.
+        match green {
+            None => {}
+            Some(side) => {
+                vec![&left_motor, &right_motor][side.val()].set_speed_sp(0)?;
+                vec![&left_motor, &right_motor][side.val()].set_speed_sp(SPEED)?;
+                sleep(Duration::from_secs(3));
+            }
+        }
 
     }
 }
